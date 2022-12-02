7a392e08c21a0f853f94afba1f64b85489b8db12
Mattia Oldani <mattiaoldani6@gmail.com>
2022-12-02 16:13:16 +0100

    Day-05 2020

---
+++ 2020/Day-05/part-02.py
@@ -0,0 +1,33 @@
+# Solution: 682
+
+with open("input.txt", "r") as f:
+    seats = list(map(lambda x : x.strip(), f.readlines()))
+
+id_ = list()
+for seat in seats:
+    rows = 128
+    max_,min_ = 127,0
+    for i in range(7):
+        rows = int(rows / 2)
+        if seat[i] == "F":
+            max_ = min_ + rows - 1
+        else:
+            min_ = min_ + rows
+    rows = max_
+
+    columns = 8
+    max_,min_ = 7,0
+    for i in range(7,10):
+        columns = int(columns / 2)
+        if seat[i] == "L":
+            max_ = min_ + columns - 1
+        else:
+            min_ = min_ + columns
+    
+    id_.append(8 * rows + max_)
+
+id_.sort()
+for i in range(1,len(id_)):
+    if id_[i] - id_[i-1] != 1:
+        print(id_[i] - 1)
+        break
