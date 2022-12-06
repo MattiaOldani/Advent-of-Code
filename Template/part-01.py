36963da5b8533022021e7f2c0e350a9f25cf44ff
Mattia Oldani <mattiaoldani6@gmail.com>
2022-12-06 07:50:54 +0100

    Template and setup script

---
+++ Template/part-01.py
@@ -0,0 +1,6 @@
+# Solution: 
+
+with open("input.txt", "r") as f:
+    data = list(map(lambda x : x.strip(), f.readlines()))
+
+print(data)
---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+day=$2
+if [[ $day == 0* ]]; then
+    day=${day:1:2}
+fi
+
+curl https://adventofcode.com/$1/day/$day/input --cookie session=$AOC --output Template/input.txt
+
+cp -r Template $1/Day-$2
+
+cat /dev/null > Template/input.txt
+
+exit
