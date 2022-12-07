07ac0a8d9106942e1351891f18b67c94d55783b8
Mattia Oldani <mattiaoldani6@gmail.com>
2022-12-07 20:07:27 +0100

    Day-06 2021

---
+++ 2021/Day-06/part-01.py
@@ -0,0 +1,20 @@
+# Solution: 355386
+
+with open("input.txt", "r") as f:
+    start = list(map(int, f.readline().strip().split(",")))
+
+fish = [[i,0] for i in range(9)]
+fish = dict(fish)
+
+for n in start:
+    fish[n] += 1
+
+for i in range(80):
+    zero = fish[0]
+    for t in range(1,9):
+        fish[t-1] = fish[t]
+    fish[8] = zero
+    fish[6] += zero
+
+total = list(map(lambda x : x[1], fish.items()))
+print(sum(total))
