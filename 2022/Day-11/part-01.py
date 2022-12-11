40fbf234e6107ecd9182ef000481b7992c5d82d5
Mattia Oldani <mattiaoldani6@gmail.com>
2022-12-11 13:47:27 +0100

    Day-11 2022

---
+++ 2022/Day-11/part-01.py
@@ -0,0 +1,42 @@
+# Solution: 57348
+
+class Monkey():
+    def __init__(self, id_, items, operation, test, true, false):
+        self.id_ = id_
+        self.items = items
+        self.operation = operation
+        self.test = test
+        self.true = true
+        self.false = false
+        self.total = 0
+
+
+with open("input.txt", "r") as f:
+    shit = list(map(lambda x : x.strip().split("\n"), f.read().split("\n\n")))
+
+monkeys = list()
+for block in shit:
+    id_ = int(block[0].strip().split(":")[0].split(" ")[1])
+    items = list(map(int, block[1].strip().split(": ")[1].split(", ")))
+    operation = block[2].strip().split(": ")[1].split(" = ")[1]
+    test = int(block[3].strip().split(" ")[3])
+    true = int(block[4].strip().split(" ")[5])
+    false = int(block[5].strip().split(" ")[5])
+    monkeys.append(Monkey(id_, items, operation, test, true, false))
+
+for i in range(20):
+    for monkey in monkeys:
+        items = monkey.items
+        monkey.total += len(items)
+        while len(items) > 0:
+            old = items.pop()
+            new = eval(monkey.operation) // 3
+            if new % monkey.test == 0:
+                monkeys[monkey.true].items.append(new)
+            else:
+                monkeys[monkey.false].items.append(new)
+    
+score = [monkey.total for monkey in monkeys]
+score.sort(reverse=True)
+
+print(score[0] * score[1])
