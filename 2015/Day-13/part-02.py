a549759e3244d36fae76e1d0511a3e6261c17712
Mattia Oldani <mattiaoldani6@gmail.com>
2023-09-01 12:18:50 +0200

    Day-13 2015

---
+++ 2015/Day-13/part-02.py
@@ -0,0 +1,43 @@
+# Solution: 601
+
+from itertools import permutations
+
+
+def evaluate(table, happiness):
+    count = 0
+    table_len = len(table)
+    for i in range(table_len):
+        current = table[i]
+        right = table[(i+1) % table_len]
+        left = table[(i-1+table_len) % table_len]
+        count += happiness[current][right]
+        count += happiness[current][left]
+    return count
+
+
+with open("input.txt", "r") as f:
+    data = list(map(lambda x : x.strip().split(" "), f.readlines()))
+
+friends = set()
+happiness = dict()
+for line in data:
+    name = line[0]
+    value = int(line[2]) if line[1] == "gain" else -int(line[2])
+    other = line[3]
+    friends.add(line[0])
+    if name not in happiness:
+        happiness[name] = dict()
+    happiness[name][other] = value
+
+happiness["Mattia"] = dict()
+for friend in friends:
+    happiness[friend]["Mattia"] = 0
+    happiness["Mattia"][friend] = 0
+
+friends.add("Mattia")
+
+best = 0
+for table in permutations(friends):
+    best = max(best, evaluate(table, happiness))
+
+print(best)
