13a1e564021aa7a1451031c2cefd5a165ef129b3
Mattia Oldani <mattiaoldani6@gmail.com>
2023-12-01 13:22:07 +0100

    Migliorata la formattazione

--- Template/part-01.py
+++ Template/part-01.py
@@ -4,3 +4,4 @@ with open("test.txt", "r") as f:
     data = list(map(lambda x : x.strip(), f.readlines()))
 
 print(data)
+
--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ fi
 
 curl https://adventofcode.com/$1/day/$day/input --cookie session=$AOC --output Template/input.txt
 cp -r Template $1/Day-$2
+
 cat /dev/null > Template/input.txt
 
 code .
