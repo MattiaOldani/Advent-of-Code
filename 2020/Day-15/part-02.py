e88b663ac315dce7e46c94cf7d4ba87e4eebece7
Mattia Oldani <mattiaoldani6@gmail.com>
2023-12-04 01:03:04 +0100

    Day-15 2020

---
+++ 2020/Day-15/part-02.py
@@ -0,0 +1,28 @@
+# Solution: 6428
+
+from collections import defaultdict
+
+
+with open("input.txt", "r") as f:
+    first_numbers = list(map(int, f.readline().strip().split(",")))
+
+numbers = defaultdict(lambda: -1)
+
+turn = 1
+last = first_numbers.pop()
+for number in first_numbers:
+    numbers[number] = turn
+    turn += 1
+
+while turn < 30000000:
+    cit = numbers[last]
+    numbers[last] = turn
+    if cit == -1:
+        last = 0
+    else:
+        difference = turn - cit
+        last = difference
+
+    turn += 1
+
+print(last)
